29943c15f74251b4c1a6e0ae6fe417bb2ad7dac3
gibsonw <gibsonw@gmail.com>
2020-05-08 19:50:58 -0300

    .

--- Py/data_cleaner.py
+++ Py/data_cleaner.py
@@ -7,9 +7,7 @@ import unicodedata
 import codecs
 
 
-pathData = "C:\\PUCRS\\Especialização\\TCC\\data"
-pathFileDaySubject =  "https://raw.githubusercontent.com/gibsonw/jurimetrics/master/data/count_day_subject.csv"
-
+#pathFileDaySubject =  "https://raw.githubusercontent.com/gibsonw/jurimetrics/master/data/count_day_subject.csv"
 #pathFileDaySubject = pathData+'\\count_day_subject.csv'
 pathFileDaySubject =  "https://raw.githubusercontent.com/gibsonw/jurimetrics/master/data/count_day_subject.csv"
 df_count_day_subject = pd.read_csv(pathFileDaySubject,sep=",",encoding='UTF-8')
@@ -27,6 +25,8 @@ lista_weekends = pd.to_datetime(df_count_day_subject[df_count_day_subject['day_w
 lista_feriados_weekends = lista_feriados.append(lista_weekends)
 df_count_day_subject['work_day'] = df_count_day_subject['judgmentDate'].apply(lambda x: x not in lista_feriados_weekends)
 df_count_day_subject['subject_decoded'] = df_count_day_subject['subject'].apply(lambda x: codecs.decode(str(x), 'unicode_escape').encode('ISO-8859-1').decode('UTF-8'))
+
+del df_count_day_subject['subject']
 
 df_count_day_subject.to_csv('C:\\PUCRS\\Especialização\\Jurimetrics\\jurimetrics\\data\\count_day_subject_decoded.csv', index=False)
 
