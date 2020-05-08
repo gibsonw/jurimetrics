32625491582e6fc78934591509b2b8665d47cc71
gibsonw <gibsonw@gmail.com>
2020-05-08 17:38:35 -0300

    add R

---
+++ R/jurimetrics.R
@@ -0,0 +1,16 @@
+
+
+packs <- c('tidyverse','pracma','rgl','car','nycflights13',
+           'maps','Rmpfr','markovchain','fpp2','matlib')
+
+
+
+install.packages(packs, dep = T)
+
+
+
+
+update.packages(ask = F)
+
+
+
